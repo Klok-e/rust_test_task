d113563b237ff20a9e34d327bb551c5b3589e80b
LokiVKlokeNaAndoke <dmytro.sokolenko@tutanota.com>
2023-04-03 19:42:53 +0300

    Add lib top-level docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,15 @@
+//! A CLI weather application written in Rust that provides current weather and historical weather
+//! data for a given location. This library module contains the top level modules for the
+//! application, including:
+//!
+//! - `cli`: Contains the CLI command definitions and parsing logic.
+//! - `configure`: Contains the logic for configuring the application with API keys for weather
+//!   service providers.
+//! - `error`: Contains the custom error types used throughout the application.
+//! - `get`: Contains the logic for fetching weather data from the chosen provider.
+//! - `providers`: Contains the provider interface and provider implementations for the
+//!   OpenWeather and WeatherAPI services.
+
 pub mod cli;
 pub mod configure;
 pub mod error;
